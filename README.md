374738760c6e0b2d067d7d43a4fd82779bcd1ef6
Carlos Wagner <wagner30023@gmail.com>
2021-06-14 10:27:54 -0300

    implementando sistema de login simples com pdo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sistema de Login PHP PDO com MySQL
+Sistema de Login em PHP PDO com MySQL -  Login System in PHP-PDO with MySQL
+
+Um simples sistema somente com a tela de login efetuando acesso pelo form
+material todo feito em php, o mesmo pode ser usado para vários projetos
+
+
+
---
+++ conexao.php
@@ -0,0 +1,34 @@
+<?php
+abstract class Conexao{
+
+    /*nesta classe só utilizo o meu user e a senha do do DB*/
+    const USER = "root";
+    const PASS = "";
+
+    private static $instance = null;
+
+    private static function conectar(){
+
+        try{
+            if(self::$instance == null):
+                /*Abaixo mostro a instancia de qual DB estou utilizando e o nome do database*/
+                $dsn = "mysql:host=localhost;dbname=meu_sistema";
+                self::$instance = new PDO($dsn, self::USER, self::PASS);
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            endif;
+        }catch (PDOException $e){
+            echo "Erro: ".$e->getMessage();
+        }
+        return self::$instance;
+    }
+
+    protected static function getDB(){
+        return self::conectar();
+    }
+
+}
+
+?>
+
+
